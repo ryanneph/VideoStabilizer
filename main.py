73828a8de9313e62de13f1762a5c0542c8e8806e
Ryan Neph <neph320@gmail.com>
2019-05-13 09:07:14 -0700

    added partial processing ranges

--- main.py
+++ main.py
@@ -12,9 +12,12 @@ import cv2
 
 parser = argparse.ArgumentParser()
 parser.add_argument('infile', metavar='in', help='input video file')
-parser.add_argument('-o', '--out', help='input video file')
+parser.add_argument('-o', '--out', default=None, help='input video file')
 parser.add_argument('-r', '--re', '--resample', type=float, dest='resample', default=1.0, help='resample with zoom factor')
+parser.add_argument('--vis', '--visualize', dest='visualize', action='store_true', help='play video with feature annotation')
 parser.add_argument('-L', '--loglevel', default='DEBUG', choices=list(logging._nameToLevel.keys()), help='set logging level')
+parser.add_argument('--fseg', '--frame-segment', type=int, dest='fsegment', nargs=2, default=None, help='start and end frames for processing')
+parser.add_argument('--tseg', '--time-segment', type=float, dest='tsegment', nargs=2, default=None, help='start and end times for processing')
 args = parser.parse_args()
 loglevel = logging._nameToLevel[args.loglevel]
 
@@ -22,11 +25,16 @@ logger = logging.getLogger(__name__)
 logger.addHandler( logging.StreamHandler(sys.stdout) )
 logging.getLogger().setLevel(loglevel)
 
-
-def read_all_frames(cv2_cap: cv2.VideoCapture):
+def read_all_frames(cv2_cap: cv2.VideoCapture, segment=None):
     """read all frames of a cv2 VideoCapture into 3D numpy array"""
     varr = []
+    ii = -1
     while cv2_cap.isOpened():
+        ii += 1
+        if segment is not None and ii<segment[0]:
+            continue
+        elif segment is not None and ii>segment[1]:
+            break
         status, frame = cv2_cap.read()
         if not status:
             break
@@ -73,7 +81,7 @@ def interactive_play_video(varr, framerate=None, features=None):
 
     ii = 0
     while ii<varr.shape[0]:
-        frame = varr[ii]
+        frame = varr[ii].copy()
         pts = features[ii]
         for pt in pts:
             cv2.circle(frame, (pt[0], pt[1]), 5, (0,0,255), -1)
@@ -90,7 +98,7 @@ def interactive_play_video(varr, framerate=None, features=None):
                 playing = not playing
                 break
             elif keyp==ord('q') :
-                sys.exit(0)
+                return
             elif playing:
                 break
         if ii == varr.shape[0]-1:
@@ -100,27 +108,52 @@ def interactive_play_video(varr, framerate=None, features=None):
 
 
 if __name__ == '__main__':
+    time_start = time.perf_counter()
+
     vs = cv2.VideoCapture(args.infile)
     nframes = int(vs.get(cv2.CAP_PROP_FRAME_COUNT))
     hh      = int(vs.get(cv2.CAP_PROP_FRAME_HEIGHT))
     ww      = int(vs.get(cv2.CAP_PROP_FRAME_WIDTH))
     frate   = int(vs.get(cv2.CAP_PROP_FPS))
 
-    logger.debug('Read video file:\n' + \
+    logger.debug('Video Metadata:\n' + \
                  '  Frames:     {}\n'.format(nframes) + \
                  '  Size (HxW): {}x{}\n'.format(hh, ww) + \
-                 '  Rate (fps): {}\n'.format(frate) )
+                 '  Rate (fps): {}'.format(frate) )
 
-    varr = read_all_frames(vs)
+    segment = None
+    if args.fsegment or args.tsegment:
+        if args.tsegment:
+            segment = ( int(args.tsegment[0]//(1/frate)), int(args.tsegment[1]//(1/frate)) )
+        else:
+            segment = fsegment
+        segment = (max(0, segment[0]), min(nframes, segment[1]))
+        logger.debug('  Segment:    {}-{}'.format(*segment))
+    logger.debug('')
+
+    logger.info('Reading video frames...')
+    varr = read_all_frames(vs, segment=segment)
     vz = resample_all_frames(varr, args.resample)
     vg = grayscale_all_frames(varr)
 
+    logger.info('Calculating tracking features...')
     features = calculate_features(vg)
+    logger.info('Calculating camera motion...')
     camera_motion = calculate_camera_motion(features)
 
-    #  plt.imshow(vg[0], cmap='gray')
-    #  print(features.shape, features[0])
-    #  plt.scatter(features[0, :, 0], features[0, :, 1], marker='+', color='red')
-    #  plt.show()
+    if args.visualize:
+        #  plt.imshow(vg[0], cmap='gray')
+        #  print(features.shape, features[0])
+        #  plt.scatter(features[0, :, 0], features[0, :, 1], marker='+', color='red')
+        #  plt.show()
+        interactive_play_video(vz, features=features, framerate=frate)
 
-    interactive_play_video(vz, features=features, framerate=frate)
+    # save video out
+    logger.info('Saving stabilized footage...')
+    if args.out is None:
+        outfile = 'stabilized.mp4'
+    vout = cv2.VideoWriter(outfile, cv2.VideoWriter_fourcc(*'mp4v'), frate, (ww, hh))
+    for f in vz:
+        vout.write(f)
+    logger.info('Stabilized footage saved to "{}"'.format(outfile))
+    logger.debug('Total runtime: {:0.2f}s'.format(time.perf_counter()-time_start))
